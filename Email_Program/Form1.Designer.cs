68da945d769de47ce24d222b95d6eb1d7225e590
Alex-Pedersen <1038281@ucn.dk>
2016-02-26 22:22:42 +0100

    Little bit of polishing

--- Email_Program/Form1.Designer.cs
+++ Email_Program/Form1.Designer.cs
@@ -33,6 +33,7 @@ namespace Email_Program
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -207,9 +208,10 @@ namespace Email_Program
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.ShowIcon = false;
-            this.Text = "Email Forwarder V0.1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cerad\'s Mail Program";
             this.ResumeLayout(false);
             this.PerformLayout();
 
